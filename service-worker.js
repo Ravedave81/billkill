50bf68d47ce10cad7b02a0cada735bb648084188
Ravedave81 <dbmann@web.de>
2026-05-09 04:35:14 -0700

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE = "rechnungsapp-v2";
+
+const urls = [
+"/",
+"/index.html",
+"/style.css",
+"/app.js",
+"/manifest.json",
+"/assets/fussnote.svg",
+"/icons/icon.svg"
+];
+
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>cache.addAll(urls))
+);
+});
+
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r || fetch(e.request))
+);
+});
