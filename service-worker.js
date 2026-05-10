3b6f7fc5d667f61dc43ac6caeea2eba0799490c2
Ravedave81 <dbmann@web.de>
2026-05-11 00:38:54 +0200

    Revert "Add files via upload"
    This reverts commit 86979a7d1709c951e440f3f88146bec184d7e9ba.

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,23 @@
-const CACHE_NAME = 'billkill-v2';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/assets/fussnote.svg'
+const CACHE = "rechnungsapp-v5";
+
+const urls = [
+"./",
+"index.html",
+"style.css",
+"app.js",
+"manifest.json",
+"assets/fussnote.svg",
+"icons/icon.svg"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
+self.addEventListener("install", e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>cache.addAll(urls))
+);
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
-  );
+self.addEventListener("fetch", e=>{
+e.respondWith(
+caches.match(e.request).then(r=>r || fetch(e.request))
+);
 });
